c34e44ce7e934d96b4c3500efccce180f449d7cb
Paolina Ivanova <parosag.hbusiness@gmail.com>
2022-09-24 13:04:15 +0300

    This is a solution of problems in Basic Syntax - Exercise

---
+++ Basic Syntax - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            while (secondNum >= firstNum)
+            {
+                Console.Write($"{firstNum} ");
+                sum += firstNum;
+                if (secondNum == firstNum)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Sum: {sum}");
+                }
+                firstNum++;
+
+            }
+
+        }
+    }
+}
---
+++ Basic Syntax - Exercise/05. Login/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string user = Console.ReadLine();
+            string pass = Console.ReadLine();
+            for (int i = 0; i <= 3; i++)
+            {
+                if (i == 3)
+                {
+                    if (user == ReverseString(pass))
+                    {
+                        Console.WriteLine("User {0} logged in.", user);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("User {0} blocked!", user);
+                    }
+                }
+                else if (pass == ReverseString(user))
+                {
+                    Console.WriteLine("User {0} logged in.", user);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    pass = Console.ReadLine();
+                }
+            }
+        }
+
+        private static string ReverseString(string pass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
